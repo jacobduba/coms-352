fc02940894c0696f954de89cdd72336da3c1b13e
Jacob Duba <jacob@mooshoe.me>
2024-11-30 17:25:16 -0600

    I believe!

--- proj2/encrypt-driver.c
+++ proj2/encrypt-driver.c
@@ -1,17 +1,21 @@
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "encrypt-module.h"
+
+#define CHAR_NULL '\0'
 
 typedef enum { EMPTY, UNCOUNTED, COUNTED } flag_t;
 
 typedef struct {
   char data;
   flag_t flag;
-  sem_t lock;
+  pthread_mutex_t lock;
+  pthread_cond_t unlocked;
 } buffer_slot;
+
+int input_buffer_size, output_buffer_size;
 
 buffer_slot *input_buffer;
 buffer_slot *output_buffer;
@@ -22,17 +26,29 @@ pthread_t output_counter_thread;
 pthread_t output_thread;
 // All threads wait when they run out of data
 // Wait until these signals to try again
-pthread_cond_t signal_input_thread;
-pthread_cond_t signal_input_counter_thread;
-pthread_cond_t signal_encrypt_thread;
-pthread_cond_t signal_output_counter_thread;
-pthread_cond_t signal_output_thread;
+pthread_cond_t input_thread_finished;
+pthread_cond_t input_counter_thread_finished;
+pthread_cond_t encrypt_thread_finished;
+pthread_cond_t output_counter_thread_finished;
+pthread_cond_t output_thread_finished;
 
 void reset_requested() { log_counts(); }
 
 void reset_finished() {}
 
-void *input_thread_fun() {}
+void *input_thread_fun() {
+  char c;
+  int i;
+
+  for (i = 0;; i = (i + 1) % input_buffer_size) {
+    c = read_input();
+
+    sem_wait(&input_buffer[i].lock);
+    // if (input_buffer[i])
+
+    if (c == EOF) return;
+  }
+}
 
 void *input_counter_thread_fun() {}
 
@@ -43,7 +59,6 @@ void *output_counter_thread_fun() {}
 void *output_thread_fun() {}
 
 int main(int argc, char *argv[]) {
-  int input_buffer_size, output_buffer_size;
   char c;
 
   if (argc != 4) {
@@ -73,17 +88,23 @@ int main(int argc, char *argv[]) {
   }
 
   for (int i = 0; i < input_buffer_size; i++) {
-    input_buffer[i].data = NULL;
+    input_buffer[i].data = CHAR_NULL;
     input_buffer[i].flag = EMPTY;
     sem_init(&input_buffer[i].lock, 0, 1);
   }
   for (int i = 0; i < output_buffer_size; i++) {
-    output_buffer[i].data = NULL;
+    output_buffer[i].data = CHAR_NULL;
     output_buffer[i].flag = EMPTY;
     sem_init(&output_buffer[i].lock, 0, 1);
   }
 
   init(argv[1], argv[2], argv[3]);
+
+  pthread_cond_init(&input_thread_finished, NULL);
+  pthread_cond_init(&input_counter_thread_finished, NULL);
+  pthread_cond_init(&encrypt_thread_finished, NULL);
+  pthread_cond_init(&output_counter_thread_finished, NULL);
+  pthread_cond_init(&output_thread_finished, NULL);
 
   pthread_create(&input_thread, NULL, input_thread_fun, NULL);
   pthread_create(&input_counter_thread, NULL, input_counter_thread_fun, NULL);
@@ -96,13 +117,6 @@ int main(int argc, char *argv[]) {
   pthread_join(encrypt_thread, NULL);
   pthread_join(output_counter_thread, NULL);
   pthread_join(output_thread, NULL);
-
-  while ((c = read_input()) != EOF) {
-    count_input(c);
-    c = encrypt(c);
-    count_output(c);
-    write_output(c);
-  }
 
   printf("End of file reached.\n");
 
