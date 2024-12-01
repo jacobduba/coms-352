2314e44c0b6e399819e6616915d39bb24bac0369
Jacob Duba <jacob@mooshoe.me>
2024-11-30 19:16:21 -0600

    Oh yeah

--- proj2/encrypt-driver.c
+++ proj2/encrypt-driver.c
@@ -6,64 +6,167 @@
 
 #define CHAR_NULL '\0'
 
-typedef enum { EMPTY, UNCOUNTED, COUNTED } flag_t;
+typedef enum { EMPTY, UNCOUNTED, COUNTED } b_status_t;
+#define NUM_FLAGS 3
 
 typedef struct {
   char data;
-  flag_t flag;
+  b_status_t status;
   pthread_mutex_t lock;
-  pthread_cond_t unlocked;
-} buffer_slot;
+  pthread_cond_t status_set_to[NUM_FLAGS];
+} buffer_slot_t;
 
 int input_buffer_size, output_buffer_size;
 
-buffer_slot *input_buffer;
-buffer_slot *output_buffer;
+buffer_slot_t *input_buffer;
+buffer_slot_t *output_buffer;
 pthread_t input_thread;
 pthread_t input_counter_thread;
 pthread_t encrypt_thread;
 pthread_t output_counter_thread;
 pthread_t output_thread;
-// All threads wait when they run out of data
-// Wait until these signals to try again
-pthread_cond_t input_thread_finished;
-pthread_cond_t input_counter_thread_finished;
-pthread_cond_t encrypt_thread_finished;
-pthread_cond_t output_counter_thread_finished;
-pthread_cond_t output_thread_finished;
 
 void reset_requested() { log_counts(); }
 
 void reset_finished() {}
 
 void *input_thread_fun() {
+  buffer_slot_t *b;
   char c;
   int i;
 
   for (i = 0;; i = (i + 1) % input_buffer_size) {
+    b = &input_buffer[i];
     c = read_input();
 
-    sem_wait(&input_buffer[i].lock);
-    // if (input_buffer[i])
+    pthread_mutex_lock(&b->lock);
+    while (b->status != EMPTY)
+      pthread_cond_wait(&b->status_set_to[EMPTY], &b->lock);
+    b->data = c;
+    b->status = UNCOUNTED;
+    printf("Input thread: %c\n", c);
+    pthread_mutex_unlock(&b->lock);
+    pthread_cond_signal(&b->status_set_to[UNCOUNTED]);
 
     if (c == EOF) return;
   }
 }
 
-void *input_counter_thread_fun() {}
+void *input_counter_thread_fun() {
+  buffer_slot_t *b;
+  char c;
+  int i;
 
-void *encrypt_thread_fun() {}
+  for (i = 0;; i = (i + 1) % input_buffer_size) {
+    b = &input_buffer[i];
 
-void *output_counter_thread_fun() {}
+    pthread_mutex_lock(&b->lock);
+    while (b->status != UNCOUNTED)
+      pthread_cond_wait(&b->status_set_to[UNCOUNTED], &b->lock);
+    c = b->data;
+    b->status = COUNTED;
+    printf("Input counter thread: %c\n", c);
+    pthread_mutex_unlock(&b->lock);
+    pthread_cond_signal(&b->status_set_to[COUNTED]);
 
-void *output_thread_fun() {}
+    count_input(c);
+
+    if (c == EOF) return;
+  }
+}
+
+void *encrypt_thread_fun() {
+  buffer_slot_t *ib, *ob;
+  char c;
+  int i, j;
+
+  i = 0, j = 0;
+  for (;;) {
+    ib = &input_buffer[i];
+
+    pthread_mutex_lock(&ib->lock);
+    while (ib->status != COUNTED)
+      pthread_cond_wait(&ib->status_set_to[COUNTED], &ib->lock);
+    c = ib->data;
+    ib->status = EMPTY;
+    pthread_mutex_unlock(&ib->lock);
+    pthread_cond_signal(&ib->status_set_to[EMPTY]);
+
+    i = (i + 1) % input_buffer_size;
+
+    if (c != EOF) c = encrypt(c);
+
+    printf("Encrypt thread: %c\n", c);
+
+    ob = &output_buffer[j];
+
+    pthread_mutex_lock(&ob->lock);
+    while (ob->status != EMPTY)
+      pthread_cond_wait(&ob->status_set_to[EMPTY], &ob->lock);
+    ob->data = c;
+    ob->status = UNCOUNTED;
+    pthread_mutex_unlock(&ob->lock);
+    pthread_cond_signal(&ob->status_set_to[UNCOUNTED]);
+
+    j = (j + 1) % output_buffer_size;
+
+    if (c == EOF) return;
+  }
+}
+
+void *output_counter_thread_fun() {
+  buffer_slot_t *b;
+  char c;
+  int i, j;
+
+  for (i = 0;; i = (i + 1) % output_buffer_size) {
+    b = &output_buffer[i];
+
+    pthread_mutex_lock(&b->lock);
+    while (b->status != UNCOUNTED)
+      pthread_cond_wait(&b->status_set_to[UNCOUNTED], &b->lock);
+    c = b->data;
+    b->status = COUNTED;
+    printf("Output counter thread: %c\n", c);
+    pthread_mutex_unlock(&b->lock);
+    pthread_cond_signal(&b->status_set_to[COUNTED]);
+
+    count_output(c);
+
+    if (c == EOF) return;
+  }
+}
+
+void *output_thread_fun() {
+  buffer_slot_t *b;
+  char c;
+  int i, j;
+
+  for (i = 0;; i = (i + 1) % output_buffer_size) {
+    b = &output_buffer[i];
+
+    pthread_mutex_lock(&b->lock);
+    while (b->status != COUNTED)
+      pthread_cond_wait(&b->status_set_to[COUNTED], &b->lock);
+    c = b->data;
+    b->status = EMPTY;
+    printf("Output thread: %c\n", c);
+    pthread_mutex_unlock(&b->lock);
+    pthread_cond_signal(&b->status_set_to[EMPTY]);
+
+    if (c == EOF) return;
+
+    write_output(c);
+  }
+}
 
 int main(int argc, char *argv[]) {
   char c;
 
   if (argc != 4) {
     printf(
-        "Error: incorrect number of params.\nUsage: ./encrypt <input> <output> "
+        "Error: incorrect number of params.\nUsage: ./encrypt <input> "
+        "<output> "
         "<log>\n");
     return 1;
   }
@@ -79,9 +182,9 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  if ((input_buffer = malloc(input_buffer_size * sizeof(buffer_slot))) ==
+  if ((input_buffer = malloc(input_buffer_size * sizeof(buffer_slot_t))) ==
           NULL ||
-      (output_buffer = malloc(output_buffer_size * sizeof(buffer_slot))) ==
+      (output_buffer = malloc(output_buffer_size * sizeof(buffer_slot_t))) ==
           NULL) {
     fprintf(stderr, "Memory allocation failed\n");
     return 1;
@@ -89,28 +192,26 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < input_buffer_size; i++) {
     input_buffer[i].data = CHAR_NULL;
-    input_buffer[i].flag = EMPTY;
-    sem_init(&input_buffer[i].lock, 0, 1);
+    input_buffer[i].status = EMPTY;
+    pthread_mutex_init(&input_buffer[i].lock, NULL);
+    for (int i = 0; i < NUM_FLAGS; i++)
+      pthread_cond_init(&input_buffer[i].status_set_to[i], NULL);
   }
   for (int i = 0; i < output_buffer_size; i++) {
     output_buffer[i].data = CHAR_NULL;
-    output_buffer[i].flag = EMPTY;
-    sem_init(&output_buffer[i].lock, 0, 1);
+    output_buffer[i].status = EMPTY;
+    pthread_mutex_init(&output_buffer[i].lock, NULL);
+    for (int i = 0; i < NUM_FLAGS; i++)
+      pthread_cond_init(&input_buffer[i].status_set_to[i], NULL);
   }
 
   init(argv[1], argv[2], argv[3]);
-
-  pthread_cond_init(&input_thread_finished, NULL);
-  pthread_cond_init(&input_counter_thread_finished, NULL);
-  pthread_cond_init(&encrypt_thread_finished, NULL);
-  pthread_cond_init(&output_counter_thread_finished, NULL);
-  pthread_cond_init(&output_thread_finished, NULL);
 
   pthread_create(&input_thread, NULL, input_thread_fun, NULL);
   pthread_create(&input_counter_thread, NULL, input_counter_thread_fun, NULL);
   pthread_create(&encrypt_thread, NULL, encrypt_thread_fun, NULL);
   pthread_create(&output_counter_thread, NULL, output_counter_thread_fun, NULL);
-  pthread_create(&output_thread, NULL, output_counter_thread_fun, NULL);
+  pthread_create(&output_thread, NULL, output_thread_fun, NULL);
 
   pthread_join(input_thread, NULL);
   pthread_join(input_counter_thread, NULL);
